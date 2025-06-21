03997c3f8ff07e91a345cba5104e1944534aaa9d
dilmithp <dilmithp@outlook.com>
2025-06-21 08:41:28 +0530

    envConfig

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 import express from "express";
 
+import {PORT} from "./config/env.js";
+
 const app = express();
 
 app.get("/",(req,res)=>{
     res.send("welcome to subscription demo service")
-})
+});
+
+app.listen(3000,() => {
+    console.log(`Sub Tracker running on http://localhost:${PORT}`);
+});
+
+export default app;
