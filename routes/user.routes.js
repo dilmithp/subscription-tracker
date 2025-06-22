2301c56adc064350192b2f6b6387bab41861a5e0
dilmithp <dilmithp@outlook.com>
2025-06-22 07:57:44 +0530

    UserRoute

---
+++ routes/user.routes.js
@@ -0,0 +1,14 @@
+import {Router} from 'express';
+
+const userRouter = Router();
+
+userRouter.get('/', (req, res) => res.send({title:'GET All Users'}));
+
+userRouter.get('/:id', (req, res) => res.send({title:'GET User Users'}));
+
+userRouter.post('/', (req, res) => res.send({title:'Create a User'}));
+
+userRouter.put('/:id', (req, res) => res.send({title:'Update a user'}));
+
+userRouter.delete('/:id', (req, res) => res.send({title:'Delete a User'}));
+
