2b20166529e7c97210e244592deeb73b2f2efc1d
dilmithp <dilmithp@outlook.com>
2025-06-22 18:38:57 +0530

    authentication signup controller

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 import express from "express";
+import coockieParser from "cookie-parser";
 
 import {PORT} from "./config/env.js";
 import userRouter from "./routes/user.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import authRouter from "./routes/auth.routes.js";
 import connectDB from "./database/mongoDB.js";
+import errorMiddleware from "./middlewares/error.middleware.js";
 
 const app = express();
+// Middleware from express.js
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(coockieParser());
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/subscription', subscriptionRouter);
 app.use('/api/v1/user', userRouter);
+app.use(errorMiddleware);
 
 app.get("/",(req,res)=>{
     res.send("welcome to subscription demo service")
--- middlewares/error.middleware.js
+++ middlewares/error.middleware.js
@@ -1,30 +1,30 @@
 const errorMiddleware = (err, req, res, next) => {
-    try{
-        let error = {...err};
-
+    try {
+        let error = { ...err };
         error.message = err.message;
-        console.error(err);
+        console.error('Error caught by middleware:', err);
 
-        //mongoose bad object id
-        if(err.name === "CastError"){
-            const message = `Resource not found. Invalid: ${err.path}`;
-            error = new Error(message);
+        // Mongoose errors...
+        if (err.name === "CastError") {
+            error = new Error(`Resource not found. Invalid: ${err.path}`);
             error.statusCode = 404;
         }
-        //mongoose duplicate key
-        if(err.code === 11000){
-            const message = 'Duplicate field value entered';
+        if (err.code === 11000) {
+            error = new Error('Duplicate field value entered');
+            error.statusCode = 400;
+        }
+        if (err.name === "ValidationError") {
+            const message = Object.values(err.errors).map(val => val.message).join(', ');
             error = new Error(message);
             error.statusCode = 400;
         }
-        //mongoose validation error
-        if(err.name === "ValidationError"){
-            const message = Object.values(err.errors).map(val => val.message);
-            error = new Error(message.join(', '));
-            error.statusCode = 400;
-        }
-        res.status(err.statusCode || 500).json({sucess:false, error: error.message || "Server Error", status: err.statusCode || 500});
-    }catch(e){
+        res.status(error.statusCode || 500).json({
+            success: false,
+            error: error.message || "Server Error",
+            status: error.statusCode || 500
+        });
+    } catch (e) {
         next(e);
     }
-}
+};
+export default errorMiddleware;
